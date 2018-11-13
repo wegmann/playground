47d442be375acca0934fadeb41722628c8a284a3
zoo3143 <zoo3143@wfhn073.uni-wuerzburg.eu>
2018-11-13 19:22:14 +0100

    minor changes

--- ggplot_cube.R
+++ ggplot_cube.R
@@ -7,7 +7,7 @@ obj <- threed::mesh3dobj$cube
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Define camera 'lookat' matrix i.e. camera-to-world transform
+# Define camera 'look at' matrix i.e. camera-to-world transform
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 camera_to_world <- threed::look_at_matrix(eye = c(-1.5, 1.75, 4), at = c(0, 0, 0))
 
@@ -18,17 +18,24 @@ obj <- obj %>%
   transform_by(invert_matrix(camera_to_world)) %>%
   perspective_projection()
 
+# ##################
+# create actual plots
+
 ggplot(obj, aes(x, y)) +
   geom_point() +
   theme_void() +
   theme(legend.position = 'none') +
   coord_equal()
 
+# just lines
+
 ggplot(obj, aes(x, y, group = element_id)) +
   geom_polygon(fill = NA, colour = 'black', size = 0.2) +
   theme_void() +
   theme(legend.position = 'none') +
   coord_equal()
+
+# dotted lines in the back
 
 ggplot(obj, aes(x, y, group = element_id)) +
   geom_polygon(fill = NA, colour='black', aes(linetype = hidden,  size = hidden)) +
@@ -38,6 +45,7 @@ ggplot(obj, aes(x, y, group = element_id)) +
   theme(legend.position = 'none') +
   coord_equal()
 
+# colored sides
 ggplot(obj, aes(x, y, group = zorder)) +
   geom_polygon(aes(fill = fny + fnz), colour = 'black', size = 0.2) +
   theme_void() +
@@ -49,7 +57,6 @@ ggplot(obj, aes(x, y, group = zorder)) +
 # plotting a bunny
 #  ############
 
-camera_to_world <- look_at_matrix(eye = c(-1.5, 1.75, 4), at = c(0, 0, 0))
 
 obj <- threed::mesh3dobj$bunny %>%
   transform_by(invert_matrix(camera_to_world)) %>%
