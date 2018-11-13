fa3d4310c58aa930567f2e26ecd226c764ef2d73
zoo3143 <zoo3143@wfhn073.uni-wuerzburg.eu>
2018-11-13 16:02:09 +0100

    initial commit

---
+++ ggplot_cube.R
@@ -0,0 +1,45 @@
+library(ggplot2)
+library(threed)
+library(dplyr)
+
+obj <- threed::mesh3dobj$cube
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Define camera 'lookat' matrix i.e. camera-to-world transform
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+camera_to_world <- threed::look_at_matrix(eye = c(1.5, 1.75, 4), at = c(0, 0, 0))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Transform the object into camera space and do perspective projection
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+obj <- obj %>%
+  transform_by(invert_matrix(camera_to_world)) %>%
+  perspective_projection()
+
+ggplot(obj, aes(x, y)) +
+  geom_point() +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+ggplot(obj, aes(x, y, group = element_id)) +
+  geom_polygon(fill = NA, colour = 'black', size = 0.2) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+ggplot(obj, aes(x, y, group = element_id)) +
+  geom_polygon(fill = NA, colour='black', aes(linetype = hidden,  size = hidden)) +
+  scale_linetype_manual(values = c('TRUE' = "dotted", 'FALSE' = 'solid')) +
+  scale_size_manual(values = c('TRUE' = 0.2, 'FALSE' = 0.5)) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+ggplot(obj, aes(x, y, group = zorder)) +
+  geom_polygon(aes(fill = fny + fnz), colour = 'black', size = 0.2) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+
