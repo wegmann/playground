365c0ad70983e25d897757fb3dbce8d828c5421c
zoo3143 <zoo3143@wfhn073.uni-wuerzburg.eu>
2018-11-13 19:11:51 +0100

    added another feature

--- ggplot_cube.R
+++ ggplot_cube.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+# devtools::install_github("coolbutuseless/threed")
 library(threed)
 library(dplyr)
 
@@ -7,7 +8,7 @@ obj <- threed::mesh3dobj$cube
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Define camera 'lookat' matrix i.e. camera-to-world transform
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-camera_to_world <- threed::look_at_matrix(eye = c(1.5, 1.75, 4), at = c(0, 0, 0))
+camera_to_world <- threed::look_at_matrix(eye = c(-1.5, 1.75, 4), at = c(0, 0, 0))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Transform the object into camera space and do perspective projection
@@ -43,3 +44,42 @@ ggplot(obj, aes(x, y, group = zorder)) +
   coord_equal()
 
 
+# ##################
+# plotting a bunny
+#  ############
+
+camera_to_world <- look_at_matrix(eye = c(-1.5, 1.75, 4), at = c(0, 0, 0))
+
+obj <- threed::mesh3dobj$bunny %>%
+  transform_by(invert_matrix(camera_to_world)) %>%
+  perspective_projection()
+
+ggplot(obj, aes(x, y, group = element_id)) +
+  geom_polygon(aes(fill = fnx + fny, colour = fnx + fny, group = zorder)) +
+  theme_minimal() +
+  theme(
+    legend.position = 'none',
+    axis.text       = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+  ) +
+  coord_equal() 
+
+ggplot(obj, aes(x, y, group = element_id)) +
+  geom_polygon(fill = NA, colour='black', aes(linetype = hidden,  size = hidden)) +
+  scale_linetype_manual(values = c('TRUE' = "dotted", 'FALSE' = 'solid')) +
+  scale_size_manual(values = c('TRUE' = 0.2, 'FALSE' = 0.5)) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+ggplot(obj, aes(x, y, group = zorder)) +
+  geom_polygon(aes(fill = fny + fnz), colour = 'black', size = 0.2) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_equal()
+
+# plot a cow, a teapot, a sphere ... 
+
+
+# more here https://github.com/coolbutuseless/threed
